7ad82b10d199cb08f17ae777e9f93ba891c18026
aminudangaata <tijani.aminu@gmail.com>
2023-12-22 16:49:04 +0000

    fixtures for PuSMAG

--- pusmag/hooks.py
+++ pusmag/hooks.py
@@ -24,6 +24,10 @@ website_route_rules = [
     }
 ]
 
+fixtures = [
+    {"dt": "PS Blog Post"},
+    {"dt": "PS Gallery"}
+]
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "pusmag/public/scss/website"
 
